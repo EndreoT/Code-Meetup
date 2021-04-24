55eeaf7d0c8b1cc3a40d94afcec90009ea2c6c69
Tristan Endreo <endreotm@gmail.com>
2021-04-23 20:26:49 -0700

    fix: title correctly displays on large screens

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -42,10 +42,7 @@ class HomePage extends React.Component {
       <div>
         <div>
           <Jumbotron className="jumbotron__homepage" fluid>
-            <Container id="bannerText">
-              {/* <a href="https://fontmeme.com/fonts/dystopian-future-font/"><img src="https://fontmeme.com/permalink/190806/ad71f4b6a60c7a3e01444c7601f8d78d.png" alt="dystopian-future-font" border="0"/></a>               */}
-              {/* <h1 className="brand" style={{color: 'white', marginTop: '3rem', fontSize: '4rem'}}>
-            <img src="https://fontmeme.com/permalink/190807/6aadb61abb6dff195b588f6482f2ac8c.png" alt="avayx-font" border="0"/>            */}
+            <Container id="bannerText" fluid>
               <h1 className="brand" style={{color: 'white', marginTop: '4.5rem', fontSize: '8vw'}}>
                 <div id="title">
                   <span>&#60;</span>
@@ -55,12 +52,6 @@ class HomePage extends React.Component {
                 </div>
               </h1>
               <h3 id="titleDesc" style={{fontSize: '3.3vw', marginTop: '.6rem'}}>A meet up application where you can create events to network and code.</h3>
-              {/* <p className="brand" id="desc"> */}
-              {/* <a href="https://fontmeme.com/fonts/dystopian-future-font/"><img src="https://fontmeme.com/permalink/190806/924bfc3a28aa3825234a2dca46d1eaec.png" alt="dystopian-future-font" border="0"/></a>              </p> */}
-              {/* </h1>
-              <p className="brand" id="desc">
-              <img src="https://fontmeme.com/permalink/190807/75ad7431bbb8a02c82d3aa34a4436fc7.png" alt="planet-n-compact-font" border="0"/>
-              </p> */}
             </Container>
           </Jumbotron>
         </div>
